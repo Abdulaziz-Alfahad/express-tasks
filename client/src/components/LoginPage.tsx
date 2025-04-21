857e7c5966024c10a552ea1fd0d2da81ac1679ed
azoz14330 <107814501+azoz14330@users.noreply.github.com>
2025-04-21 18:09:37 +0300

    Added an unstylized login page

---
+++ client/src/components/LoginPage.tsx
@@ -0,0 +1,55 @@
+import axios from "axios";
+import { useState } from "react";
+
+function LoginPage(){
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState<String | null>(null);
+    
+    const handleSubmit = async (e: React.FormEvent)=>{
+        e.preventDefault();
+        setError(null);
+        try{
+            const response = await axios.post('http://localhost:5000/user/login', {
+                username,
+                password
+            })
+            const { token } = response.data;
+            localStorage.setItem('token', token);
+            alert("You are signed in")
+        }catch(err:any){
+            console.log("Sign in error:", err);
+            if(err.response){
+                setError(err.response.data.message || 'Invalid email or password.');
+            }
+            else{
+                setError("An error occured, please try again later.")
+            }
+        }
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>Sign in form</h2>
+            <div>
+                <label>Username:</label>
+                <input 
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                 />
+            </div>
+            <div>
+                <label>Password:</label>
+                <input 
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                />
+            </div>
+            {error && <p style={{color: 'red'}}>{error}</p>}
+            <button type="submit">Login</button>
+        </form>
+    );
+}
+
+export default LoginPage;
